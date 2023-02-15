9efb2f0ee320b513b3967851f3f4cc4d726faef0
Xavier <x.nunez@miautobox.com>
2023-02-15 16:56:14 -0400

    centralizando los iconos

--- proyect/src/views/login/index.vue
+++ proyect/src/views/login/index.vue
@@ -2,40 +2,86 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-icon onload="getIcons('mailOutline')"></ion-icon>
+        <!-- <ion-icon ref="iconsx" id="eye"></ion-icon> -->
+        <ion-icon role="img" class="md" id="arrowBackSharp">
+          <div class="icon-inner">
+            <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path stroke-linecap="square" stroke-miterlimit="10" stroke-width="48" d="M244 400L100 256l144-144M120 256h292" class="ionicon-fill-none"></path></svg>
+          </div>
+        </ion-icon>
+        <!-- <ion-icon onload="getIcons('mailOutline')"></ion-icon> -->
         <!-- <ion-icon :src="icon('mailOutline')"></ion-icon> -->
+        <!-- <ion-icon id="icons" data="eye"></ion-icon> -->
         <ion-title>Log In</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content>
       <div id="container" style="position: relative;">
         <form>
-          <ion-item lines="full">
-            <ion-label position="floating">Email</ion-label>
-            <ion-input type="email" required></ion-input>
+          <ion-list>
+            <ion-item>
+              <ion-label position="stacked">Enter Email id / Mobile Number</ion-label>
+              <ion-input fill="outline" type="email" required></ion-input>
+            </ion-item>
+          </ion-list>
+          <ion-list>
+            <ion-item>
+              <ion-label position="stacked">Enter Password</ion-label>
+              <ion-input fill="outline" type="password" required></ion-input>
+              <!-- <ion-icon name="eye" slot="end"></ion-icon> -->
+            </ion-item>
+          </ion-list>
+          <div class="space"></div>
+          <ion-item>
+            <ion-checkbox slot="start" expand="full"></ion-checkbox>
+            <ion-label>
+              <p style="font-size: 10px;">
+                By Login in you agree to our Terms & Condition and <br> Privaticy Policy 
+              </p>              
+            </ion-label>
           </ion-item>
-          <ion-item lines="full">
-            <ion-label position="floating">Password</ion-label>
-            <ion-input type="password" required></ion-input>
-          </ion-item>
-          <ion-row>
-            <ion-col>
-              <ion-button type="submit" color="danger" expand="block">Sign In</ion-button>              
-              <ion-label router-link="/Forgot">Forgot Password?</ion-label>
-              <br><br><br>
-              <ion-label>New User?</ion-label> <br><br>
-              <ion-button router-link="/Register">Sing up</ion-button>  
-            </ion-col>
-          </ion-row>
+          <div class="space"></div>
+          <ion-list>
+            <ion-button type="submit" color="light" expand="full">Log In</ion-button>
+          </ion-list>  
+          <div class="space"></div>        
+          <ion-list>
+            <div style="text-align: center;">OR</div>
+          </ion-list>
+          <div class="space"></div>
+          <ion-list>
+            <ion-button color="danger" expand="full">Sign In with Google</ion-button>
+          </ion-list>          
+          <ion-list>
+            <ion-button color="primary" expand="full">Sign In with Facebook</ion-button>
+          </ion-list>  
+          <div class="space"></div>
+          <ion-list style="text-align: center;">
+            <ion-label>New User?</ion-label>
+            <ion-button router-link="/Register" class="btnLink">Sing Up</ion-button>  
+          </ion-list>          
         </form>
       </div>
     </ion-content>
   </ion-page>
 </template>
-<script setup lang="ts">import { IonPage, IonHeader, IonToolbar, IonIcon, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonRow, IonCol, IonButton } from '@ionic/vue';
-
-// console.log(icons['add']);
-
+<script>
+import { onMounted, ref } from "vue"
+export default {
+  setup() {
+    const iconsx = ref(null)
+    onMounted(() => {
+      const id = iconsx.value.$el.id
+      const icons = window.icons;
+      // const icon = ":ios="+icons[id]+" :md="+icons[id];
+      document.getElementById(id).setAttribute(":ios", icons[id]);
+      document.getElementById(id).setAttribute(":md", icons[id]);
+      // console.log(icon);
+    });
+    return {
+      iconsx
+    }
+  }
+};
 // console.log(window.icons);
 
 //  icon(name: any) {
@@ -44,10 +90,10 @@
 //   return this.i[name];
 //   window.icons
 // }
-function getIcons(icon: any): string {
-    const icons = window.icons;
-    return ":ios="+icons[icon]+" :md="+icons[icon];
-}
+// function getIcons(icon: any): string {
+//     const icons = window.icons;
+//     return ":ios="+icons[icon]+" :md="+icons[icon];
+// }
 // export default {
 
 // };
@@ -55,11 +101,33 @@ function getIcons(icon: any): string {
 </script>
 
 <style scoped>
+.btnLink {
+  --background: rgb(255, 255, 255);
+  --background-hover: rgb(255, 255, 255);
+  --background-activated: rgb(255, 255, 255);
+  --background-focused: rgb(255, 255, 255);
+  --color: #000;
+  --border-radius: 0;
+  --border-color: rgb(255, 255, 255);
+  --border-style: solid;
+  --border-width: 1px;
+  --box-shadow: rgb(255, 255, 255);
+  --padding-top: -10px;
+  --padding-bottom: 10px;
+  text-decoration: underline !important;
+}
+.scroll-y {
+    overflow-y: hidden !important;
+}
+.space {
+  height: 25px;
+}
 #container {
   position: absolute;
-  top: 50%;
-  left: 10%;
-  margin: -25px 0 0 -25px;
+  top: 45%;
+  /* left: 10%; */
+  /* margin: -25px 0 0 -25px; */
+  margin: 15px 0 0 20px;
   transform: translateY(-50%);
   width: 90%;
 }
