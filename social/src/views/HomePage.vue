a313a4d6c013c8f58f8741f4fe10d3b1df6dd38a
Xavier Nuñez <xhnl21@gmail.com>
2023-02-17 00:14:50 -0400

    pruebas

--- social/src/views/HomePage.vue
+++ social/src/views/HomePage.vue
@@ -1,18 +1,36 @@
 <script setup>
-import { googleSdkLoaded } from "vue3-google-login"
+import { googleSdkLoaded, googleLogout } from "vue3-google-login"
 const login = () => {
   googleSdkLoaded((google) => {
     google.accounts.oauth2.initCodeClient({
-      client_id: '352931034399-ht1i7mqefgrbsn67a4b1nm991bvat47l.apps.googleusercontent.com',
+      client_id: '64349569579-ebh4v58m5i1v7f15t82b5p1b61grqdca.apps.googleusercontent.com',
+      // secret_client: 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
       scope: 'email profile openid',
       callback: (response) => {
         console.log("Handle the response", response)
+        console.log("Handle the response222",response.getBasicProfile())
       }
     }).requestCode()
+  })
+}
+const onSignIn = (googleUser) => {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+const yourLogoutFunction = () => {
+  // your logout logics
+  // googleLogout()
+  googleLogout((google) => {
+    console.log(google);
   })
 }
 </script>
 
 <template>
-  <button @click="login">Login Using Google</button>
+  <button @click="login">Login Using Google</button><br>
+  <button @click="yourLogoutFunction">Logout</button><br>
+  <div class="g-signin2" data-onsuccess="onSignIn"></div>
 </template>
