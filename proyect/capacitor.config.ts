870db9899a7342ba42fd5b753922908f4d29927a
Xavier <x.nunez@miautobox.com>
2023-02-18 09:25:07 -0400

    configuraciones y documentacion

--- proyect/capacitor.config.ts
+++ proyect/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'clienteMenuSide',
+  appName: 'autobox',
   webDir: 'dist',
   bundledWebRuntime: false
 };
