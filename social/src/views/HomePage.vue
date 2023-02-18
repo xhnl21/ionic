88de304649074970994636a568497d1e5305ae29
Xavier <x.nunez@miautobox.com>
2023-02-18 11:58:42 -0400

    social

--- social/src/views/HomePage.vue
+++ social/src/views/HomePage.vue
@@ -1,14 +1,40 @@
 <script setup>
-import { googleSdkLoaded, googleLogout } from "vue3-google-login"
+//  https://www.youtube.com/watch?v=qMFytZ-JI9g
+// https://yobaji.github.io/vue3-google-login/#custom-button-as-slot
+// import { onMounted } from "vue"
+// import { googleLogout, googleOneTap, googleAuthCodeLogin, googleTokenLogin } from "vue3-google-login"
+import { googleSdkLoaded } from "vue3-google-login"
+// const login = () => {
+//   googleAuthCodeLogin().then((response) => {
+//     console.log("Handle the response", response)
+//   })
+// }
+// const login = () => {
+//   googleTokenLogin().then((response) => {
+//     console.log("Handle the response", response)
+//   })
+// }
+// onMounted(() => {
+//   googleOneTap({ autoLogin: true })
+//     .then((response) => {
+//       // This promise is resolved when user selects an account from the the One Tap prompt
+//       console.log("Handle the response", response)
+//     })
+//     .catch((error) => {
+//       console.log("Handle the error", error)
+//     })
+// })
 const login = () => {
   googleSdkLoaded((google) => {
     google.accounts.oauth2.initCodeClient({
       client_id: '64349569579-ebh4v58m5i1v7f15t82b5p1b61grqdca.apps.googleusercontent.com',
       // secret_client: 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
       scope: 'email profile openid',
+      prompt: 'consent',
+      fetch_basic_profile:true,
       callback: (response) => {
         console.log("Handle the response", response)
-        console.log("Handle the response222",response.getBasicProfile())
+        console.log("Handle the userData")
       }
     }).requestCode()
   })
@@ -23,13 +49,19 @@ const onSignIn = (googleUser) => {
 const yourLogoutFunction = () => {
   // your logout logics
   // googleLogout()
-  googleLogout((google) => {
-    console.log(google);
+  googleSdkLoaded((google) => {
+    google.accounts.id.revoke("xhnl21@gmail.com")
   })
 }
-</script>
 
+// const callback = (response) => {
+//   console.log("Handle the response", response)
+// }
+</script>
 <template>
+  <!-- <div>One-Tap prompt will be shown once this component is mounted</div> -->
+  <!-- <GoogleLogin :callback="callback"/> -->
+  <br><br>
   <button @click="login">Login Using Google</button><br>
   <button @click="yourLogoutFunction">Logout</button><br>
   <div class="g-signin2" data-onsuccess="onSignIn"></div>
